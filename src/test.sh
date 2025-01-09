7db2dfb36789d57de499b223e5e8e4cd98e8f946
Tadhg O'Brien <tadhgobrien10@gmail.com>
2025-01-09 16:01:26 -0500

    env variable test

--- src/test.sh
+++ src/test.sh
@@ -1,7 +1,15 @@
-  #!/bin/bash
-  set -euo pipefail
+#!/bin/bash
 
-  NAME=$(buildkite-agent meta-data get environment-type)
+# Declare an array named 'programming_languages'
+programming_languages=('Bash' 'Python' 'JavaScript' 'Ruby', 'Java')
 
-  IFS=', ' read -r -a array <<< "$NAME"
-  echo "${array[0]}"
+# Guess the user's preferred language
+random_number=$((RANDOM % 5))
+echo "We guess your preferred language is: ${programming_languages[$random_number]}"
+
+# Display the results
+if [[ "${preferred_language}" == "${programming_languages[$random_number]}" ]]; then
+    echo "Yaay, we guessed correct, it's ${preferred_language}"
+else
+    echo "We guessed wrong, it's ${preferred_language}"
+fi
