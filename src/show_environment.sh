24edcd699c066c71b75812e6b719efb37edb838f
Tadhg O'Brien <tadhgobrien10@gmail.com>
2025-01-13 14:25:01 -0500

    update sh

--- src/show_environment.sh
+++ src/show_environment.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-(
-  set -x
+function generate_number() {
+  MAX_NUMBER=${MAX_NUMBER:-9}
+  echo $(( $RANDOM % $MAX_NUMBER + 1))
+}
 
-  env | sort
+parallel_index=${1:-}
 
-  uname -a
-)
+generated_number=$(generate_number)
 
-if [[ -x "$(command -v node)" ]]; then
-  ( set -x; node --version )
+if [[ -n "$parallel_index" ]]; then
+  ( set -x ; buildkite-agent meta-data set "generated-number::$parallel_index" "${generated_number}" )
 else
-  echo "WARN: NodeJS not installed"
+  ( set -x ; buildkite-agent meta-data set "generated-number" "${generated_number}" )
 fi
 
-if [[ -x "$(command -v yarn)" ]]; then
-  ( set -x; yarn --version )
-else
-  echo "WARN: Yarn not installed"
-fi
+echo
+echo
